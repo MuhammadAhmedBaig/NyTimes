be5eecad3b53e63b75d9854e8aeb88c1d2f5de30
Muhammad Ahmed Baig <muhammad.ahmed@airlifttech.com>
2022-07-17 03:08:48 +0500

    Working on cell subviews (continue...)

--- NyTimesArticles/NyTimesArticles/NewsFeed/TableView/Cell/NewsFeedCell.swift
+++ NyTimesArticles/NyTimesArticles/NewsFeed/TableView/Cell/NewsFeedCell.swift
@@ -9,6 +9,36 @@ import UIKit
 
 class NewsFeedCell: UITableViewCell, IdentifiableCell {
     
+    var imgView: UIImageView = {
+        let imgView = UIImageView()
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        return imgView
+    }()
+    
+    var titleLbl: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.text = "---"
+        lbl.numberOfLines = 2
+        return lbl
+    }()
+    
+    var autherLbl: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.text = "---"
+        lbl.numberOfLines = 1
+        return lbl
+    }()
+    
+    var dateLbl: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.text = "----"
+        lbl.numberOfLines = 1
+        return lbl
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -17,5 +47,13 @@ class NewsFeedCell: UITableViewCell, IdentifiableCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         // Configure the view for the selected state
+    }
+    
+    func addViews() {
+        self.backgroundColor = .white
+        self.contentView.addSubview(self.titleLbl)
+        self.contentView.addSubview(self.imgView)
+        self.contentView.addSubview(self.autherLbl)
+        self.contentView.addSubview(self.dateLbl)
     }
 }
